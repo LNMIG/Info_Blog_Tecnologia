9fa9fd7479dbf321450b1a336e7576d4fb5a575f
LNMIG <migliorenicolas@yahoo.com.ar>
2023-07-15 15:23:25 -0300

    Fix: changes in blog's code and urls file

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,4 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
@@ -8,6 +7,6 @@ urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('autor/', views.autor, name='autor'),
     path('archivo/', views.archivo, name='archivo'),
-    path('articulo/', views.categoria, name='articulo'),
+    path('articulo/', views.articulo, name='articulo'),
     path('categoria/', views.categoria, name='categoria'),
 ]
--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -131,4 +132,4 @@ STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR), 'static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
